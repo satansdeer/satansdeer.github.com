a225594a1cc282f52308e7db9c6ff0b7a75f61da
Maksim Ivanov <maxim.ivanov@toptal.com>
2018-04-09 06:19:00 +0200

    Added image to static type checing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,47 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import get from 'lodash/get'
-import Helmet from 'react-helmet'
+import React from "react";
+import Link from "gatsby-link";
+import get from "lodash/get";
+import Helmet from "react-helmet";
+import Img from "gatsby-image";
 
-import Bio from '../components/Bio'
-import { rhythm } from '../utils/typography'
+import Bio from "../components/Bio";
+import { rhythm } from "../utils/typography";
 
 class BlogIndex extends React.Component {
   render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allMarkdownRemark.edges')
+    const siteTitle = get(this, "props.data.site.siteMetadata.title");
+    const posts = get(this, "props.data.allMarkdownRemark.edges");
 
     return (
       <div>
         <Helmet title={siteTitle} />
         <Bio />
         {posts.map(({ node }) => {
-          const title = get(node, 'frontmatter.title') || node.fields.slug
+          const title = get(node, "frontmatter.title") || node.fields.slug;
+          const image = `${node.fields.slug}${get(node, "frontmatter.image")}`;
           return (
             <div key={node.fields.slug}>
               <h3
                 style={{
-                  marginBottom: rhythm(1 / 4),
+                  marginBottom: rhythm(1 / 4)
                 }}
               >
-                <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
+                <Link style={{ boxShadow: "none" }} to={node.fields.slug}>
                   {title}
                 </Link>
               </h3>
+              <Img resolutions={data.file.childImageSharp.resolutions} />
               <small>{node.frontmatter.date}</small>
               <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
             </div>
-          )
+          );
         })}
       </div>
-    )
+    );
   }
 }
 
-export default BlogIndex
+export default BlogIndex;
 
 export const pageQuery = graphql`
   query IndexQuery {
@@ -55,11 +58,20 @@ export const pageQuery = graphql`
             slug
           }
           frontmatter {
-            date(formatString: "DD MMMM, YYYY")
             title
+            date(formatString: "DD MMMM, YYYY")
+            image {
+              childImageSharp {
+                responsiveSizes(maxWidth: 400) {
+                  src
+                  srcSet
+                  sizes
+                }
+              }
+            }
           }
         }
       }
     }
   }
-`
+`;
