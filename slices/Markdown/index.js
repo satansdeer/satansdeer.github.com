e5185c4a544e091233df65304fd7385ec0c2df94
Maksim Ivanov <satansdeer@gmail.com>
2022-07-26 20:22:49 +0200

    Add prism to regular markdown

--- slices/Markdown/index.js
+++ slices/Markdown/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactMarkdown from "react-markdown";
 import * as prismicH from "@prismicio/helpers";
+import Prism from "prismjs";
+require("prismjs/components/prism-jsx");
 
 const Markdown = ({ slice }) => {
 	const text = prismicH.asText(slice.primary.text)
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
 
-  return <ReactMarkdown children={text}/>
+  return <ReactMarkdown>{text}</ReactMarkdown>
 }
 
 export default Markdown
