e0930ea19dca9a5cdfcca310a1049dc66eda02b9
Maksim Ivanov <satansdeer@gmail.com>
2023-07-08 08:20:32 +0200

    Fix missing import

--- slices/Markdown/index.js
+++ slices/Markdown/index.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import * as prismicH from "@prismicio/helpers";
 import Prism from "prismjs";
 require("prismjs/components/prism-jsx");
 
 const Markdown = ({ slice }) => {
-	const text = prismicH.asText(slice.primary.text)
+  const text = prismicH.asText(slice.primary.text);
+
   useEffect(() => {
     Prism.highlightAll();
   }, []);
 
-  return <ReactMarkdown>{text}</ReactMarkdown>
-}
+  return <ReactMarkdown>{text}</ReactMarkdown>;
+};
 
-export default Markdown
+export default Markdown;
